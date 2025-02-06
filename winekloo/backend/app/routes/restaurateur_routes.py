58fe6faca2422de9cc88f2ab399ac15d2edeb23e
imeney <120603260+imeney@users.noreply.github.com>
2025-02-06 12:41:47 +0100

    added the fetch of all restaurants

--- winekloo/backend/app/routes/restaurateur_routes.py
+++ winekloo/backend/app/routes/restaurateur_routes.py
@@ -15,35 +15,9 @@ def get_restaurateur_by_id(id):
 
 @restaurateur_bp.route('/all/restaurateurs/', methods = ['GET'])
 def get_all_restaurateurs():
-    try:
-        restaurateurs = RestaurateurRepository.get_all_restaurateurs()
+    restaurateurs = RestaurateurRepository.get_all_restaurateurs()
+    for restaurateur in restaurateurs:
+        print(restaurateur.to_json())
+    return jsonify([restaurateur.to_json() for restaurateur in restaurateurs]), 200
 
-        result = [ 
-                {
-                "RestaurateurID": restaurateur.RestaurateurID,
-                "DiningType": restaurateur.DiningType,
-                "Name": restaurateur.Name,
-                "Email": restaurateur.Email,
-                "PhoneNumber": restaurateur.PhoneNumber,
-                "Password": restaurateur.Password,
-                "Location": restaurateur.Location,
-                "Photo": restaurateur.Photo,
-                "WorkingHours": restaurateur.WorkingHours,
-                "WorkingDays": restaurateur.WorkingDays,
-                "Description": restaurateur.Description,
-                "RatingValueAverage": restaurateur.RatingValueAverage,
-                "MenuFilePDF": restaurateur.MenuFilePDF,
-                "Categories": restaurateur.Categories,
-                "DietaryPreferences": restaurateur.DietaryPreferences,
-                "SpecialFeatures": restaurateur.SpecialFeatures,
-                "Pricing": restaurateur.Pricing,
-                "verified": restaurateur.verified
-            }
-            for restaurateur in restaurateurs
-        ]
-        print(result)
-
-        return jsonify({"message": "All restaurateurs fetched successfully"}), 200
-    except Exception as e:
-        print(f"Error: {e}")  
-        return jsonify({"error": "Internal server error"}), 500
+               
